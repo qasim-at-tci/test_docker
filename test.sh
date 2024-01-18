6b478b96c42c4b57dce12318ba72d04241ca5d74
Manish N <62024503+manishnagdewani-dev@users.noreply.github.com>
2024-01-18 23:33:23 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # enable docker experimental version to allow 'squashing' feature
-echo $'{\n    "experimental": true\n}' | sudo tee -a /etc/docker/daemon.json;
+echo $'{\n    "experimental": true\n}'
 sudo service docker restart
 docker version
